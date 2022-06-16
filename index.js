2393a994600d7a85edb789ecd9a62b7076a87279
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-06-16 10:57:28 -0300

    create logic to get/post requisitions

--- index.js
+++ index.js
@@ -2,11 +2,32 @@ import express from 'express'
 import cors from 'cors'
 
 const app = express();
+app.use(express.json())
 app.use(cors());
 
 let users = [];
 let tweets = [];
 
+app.post("/sign-up", (req, res) => {
+    const data = req.body
+    users = data
+    console.log(users)
+    res.send("OK")
+})
 
+app.post("/tweets", (req, res) => {
+    const data = req.body
+    tweets.push({
+        username: data.username,
+        avatar: users.avatar,
+        tweet: data.tweet,
+    })
+    res.send("OK")
+})
+
+app.get("/tweets", (req, res) => {
+    res.send(tweets)
+})
 
 app.listen(5000);
+
